bd8a3de7d19e7a6530d2b4a7cbc567759999993b
Russ Ripley <russellripley@hotmail.com>
2016-06-25 15:56:36 -0700

    finish git kata practice

--- katas.php
+++ katas.php
@@ -6,6 +6,35 @@ function capitalize($string) {
     return strtoupper(substr($string, 0, 1)) . substr($string, 1, strlen($string));
 }
 
-echo capitalize($string);
+function reverseString($string) {
+	$reversedString = "";
+	for ($i = strlen($string)-1;$i >= 0;$i--) {
+		$letter = substr($string, $i, 1);
+		$reversedString = $reversedString . $letter;
+	}
+	return $reversedString;
+}
 
-echo $string;
+function cRaZyCaSe($string) {
+	$crazyString = '';
+	$nextIsLower = true;
+	for($i = 0;$i <= strlen($string); $i++) {
+		$letter = substr($string, $i, 1);
+		if($nextIsLower === true) {
+			$crazyString = $crazyString . strtolower($letter);
+			$nextIsLower = false;
+		} else {
+			$crazyString = $crazyString . strtoupper($letter);
+			$nextIsLower = true;
+		}
+	}
+	return $crazyString;
+}
+
+echo cRaZyCaSe('ruSSELl');
+echo cRaZyCaSe('RUSSELL');
+echo cRaZyCaSe('russell');
+echo cRaZyCaSe('RUSSell');
+
+rUsSeLl;
+
