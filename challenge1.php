05974e0864bd873665d1cef0d57dbdfb28afccf8
d00msday <russellripley@hotmail.com>
2016-06-27 00:12:48 -0700

    add spacing to song

--- challenge1.php
+++ challenge1.php
@@ -1,6 +1,6 @@
 <?php function runSong($song) {
 	while ($song >= 95) {
-		echo $song . ' bottles of beer on the wall ' . $song . ' bottles of beer. Take one down and pass it around, ';
+		echo "" . $song . ' bottles of beer on the wall ' . $song . ' bottles of beer. Take one down and pass it around, ';
 		$song--; 
 		echo $song . ' bottles of beer on the wall.';
 	}
