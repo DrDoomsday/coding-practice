5b79befdf807bd71576100aa52343356b4ea4a1f
d00msday <russellripley@hotmail.com>
2016-06-27 00:10:28 -0700

    finish challenge 1

---
+++ challenge1.php
@@ -0,0 +1,9 @@
+<?php function runSong($song) {
+	while ($song >= 95) {
+		echo $song . ' bottles of beer on the wall ' . $song . ' bottles of beer. Take one down and pass it around, ';
+		$song--; 
+		echo $song . ' bottles of beer on the wall.';
+	}
+}
+
+runSong(99);
