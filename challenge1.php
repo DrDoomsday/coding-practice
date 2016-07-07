0e9de268afd17c104a8168809b465fdde3d3cdc8
Russ Ripley <russellripley@hotmail.com>
2016-07-06 20:25:30 -0700

    fix challenge  variable

--- challenge1.php
+++ challenge1.php
@@ -1,8 +1,8 @@
-<?php function runSong($song) {
-	while ($song >= 95) {
-		echo " " . $song . ' bottles of beer on the wall ' . $song . ' bottles of beer. Take one down and pass it around, ';
-		$song--; 
-		echo $song . ' bottles of beer on the wall.';
+<?php function runSong($bottles) {
+	while ($bottles >= 95) {
+		echo " " . $bottles . ' bottles of beer on the wall ' . $bottles . ' bottles of beer. Take one down and pass it around, ';
+		$bottles--;
+		echo $bottles . ' bottles of beer on the wall.';
 	}
 }
 
