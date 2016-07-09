9473996da4cb44d316afeac778351026a4595ac1
d00msday <russellripley@hotmail.com>
2016-07-08 19:20:30 -0700

    upload russ' weekly challenge 1 code

--- challenge1.php
+++ challenge1.php
@@ -1,5 +1,5 @@
 <?php function runSong($song) {
-	while ($song >= 95) {
+	while ($song >= 0) {
 		echo " " . $song . ' bottles of beer on the wall ' . $song . ' bottles of beer. Take one down and pass it around, ';
 		$song--; 
 		echo $song . ' bottles of beer on the wall.';
