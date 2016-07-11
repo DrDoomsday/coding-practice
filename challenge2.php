a126bdba15125d3002c0b41d4f67e0e4c71bfed2
d00msday <russellripley@hotmail.com>
2016-07-10 21:18:26 -0700

    correct FizzBuzz challenge

--- challenge2.php
+++ challenge2.php
@@ -2,14 +2,11 @@
 	function printFizzBizz() {
 		for ($i = 1; $i <= 100; $i++) {
 			if ($i % 5 === 0 && $i % 3 === 0) {
-				echo "FizzBizz!";
-				echo $i;
+				echo "FizzBuzz!";
 			} elseif ($i % 5 === 0) {
-				echo "Bizz!";
-				echo $i;
+				echo "Buzz!";
 			} elseif ($i % 3 === 0) {
 				echo "Fizz!";
-				echo $i;
 			} else {
 				echo $i;
 			}
