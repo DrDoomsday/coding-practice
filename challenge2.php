38afbe848042a06fd04aae16e2edd73444839792
d00msday <russellripley@hotmail.com>
2016-07-10 19:26:50 -0700

    finish challenge2 FizzBizz

--- challenge2.php
+++ challenge2.php
@@ -3,10 +3,13 @@
 		for ($i = 1; $i <= 100; $i++) {
 			if ($i % 5 === 0 && $i % 3 === 0) {
 				echo "FizzBizz!";
+				echo $i;
 			} elseif ($i % 5 === 0) {
 				echo "Bizz!";
+				echo $i;
 			} elseif ($i % 3 === 0) {
 				echo "Fizz!";
+				echo $i;
 			} else {
 				echo $i;
 			}
