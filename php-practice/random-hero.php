fc4dff3525e37fd74f4aa510f43399a4f1cc8e2c
d00msday <russellripley@hotmail.com>
2016-09-25 23:12:36 -0700

    fixed random-hero.php so POST displays correctly

--- php-practice/random-hero.php
+++ php-practice/random-hero.php
@@ -1,6 +1,17 @@
 <?php
-	require('hero-list.php');
 	require('heroes.php');
 	/*Create if function that excludes all checked items from list that then picks a random item from the list on hero-list.php from the remaining items.
 	*/
 ?>
+<!DOCTYPE html>
+<html>
+<body>
+
+	<p> <?php 
+		if(isset($_POST["Abathur"])) {
+			echo ($_POST["Abathur"]."is most important");
+		}
+		?>
+	</p>
+</body>
+</html>
