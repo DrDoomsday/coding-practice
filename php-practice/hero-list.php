b9e57cf2016fcb838f742f3b8213956a23653a2d
d00msday <russellripley@hotmail.com>
2016-09-25 22:25:50 -0700

    create files for heroes random generator

---
+++ php-practice/hero-list.php
@@ -0,0 +1,16 @@
+<?php 
+	require('heroes.php');
+ ?>
+<!DOCTYPE html>
+<html>
+	<body>
+		<h1>Heroes List</h1>
+		<form action="random-hero.php" method="post">
+			<?php
+				foreach($heroes as $singleHero) { ?>
+					<input style="display:block"; type="checkbox" name="hero" value="1"><?php print $singleHero;
+				}?>
+			<input type="submit" value="submit">
+		</form>
+	</body>
+</html>
