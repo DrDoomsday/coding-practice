3506045d3f0aad7abdde5238ad2ce1375c514908
Russell Ripley <Russell Ripley>
2016-10-03 12:25:42 -0700

    added foreach loop to create array and php function to echo random element from array

--- php-practice/random-hero.php
+++ php-practice/random-hero.php
@@ -2,15 +2,20 @@
 	require('heroes.php');
 	/*Create if function that excludes all checked items from list that then picks a random item from the list on hero-list.php from the remaining items.
 	*/
+	foreach (isset($_POST[])) {
+		$excludeHeroes = array($_POST[]);
+	}
+	/* foreach loop to take each value in the $_POST, create an array to be compared to heroes.php array and eliminate
+	matching values */
 ?>
 <!DOCTYPE html>
 <html>
 <body>
 
 	<p> <?php 
-		if(isset($_POST["Abathur"])) {
-			echo ($_POST["Abathur"]."is most important");
-		}
+		echo array_rand($result, 1) = array_diff($excludeHeroes, $heroes);
+
+		// Comapares two arrays (excluding the same elements from the two arrays) then echos a rand element from the resulting array.
 		?>
 	</p>
 </body>
