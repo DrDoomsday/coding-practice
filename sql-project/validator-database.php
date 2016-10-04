b289d1957d8a3ac082900dc6303c974a19c6998f
doomsday <russellripley@hotmail.com>
2016-10-04 14:08:06 -0700

    start sql-project for Tom

---
+++ sql-project/validator-database.php
@@ -0,0 +1,2 @@
+<?php 
+	/* this database needs to be connected to MySQL, there are going to be 16 tables that will then export to the validation-page as one single table that can filtered for specific information. 
