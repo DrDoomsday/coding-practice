bc669e7245adaa31a68e55c9e4a915563219a6d2
doomsday <russellripley@hotmail.com>
2016-10-04 23:38:33 -0700

    fix issue where array of excluded heroes broke

--- php-practice/random-hero.php
+++ php-practice/random-hero.php
@@ -2,7 +2,7 @@
 	require('heroes.php');
 	/*Create if function that excludes all checked items from list that then picks a random item from the list on hero-list.php from the remaining items.
 	*/
-		$excludeHeroes = $_POST['name'];
+		$excludeHeroes = array($_POST['name']);
 		$result = array_diff($heroes, $excludeHeroes);
 		echo array_rand(array_flip($result), 1);
 ?>
